d485d582aededa05e64c6258f104506f956b49e9
“Deep <deepmandal9091@gmail.com>
2025-11-02 23:15:55 +0530

    add

---
+++ LEVEL 2/001 PATTERN/001 TRIANGLE/001_approach_1.c
@@ -0,0 +1,48 @@
+/*
+N = 3
+* 
+* * 
+* * * 
+
+N = 5
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+*/
+
+
+#include<stdio.h>
+
+void triangelPattern(int n) {
+    int i, j;
+    for(i=0; i<n; i++) {
+        for(j=0; j<=i; j++) 
+            printf("* ");
+        printf("\n");
+    }
+}
+
+int main(){
+    int n;
+    printf("Enter N : ");
+    scanf("%d", &n);
+
+    // pattern
+    triangelPattern(n);
+
+    return 0;
+}
+
+
+/* Output :
+Enter N : 5
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+*/
